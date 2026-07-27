5d9bacde31d02a1ccc1a2176f00fd5e97690ab44
Andrey_Beloboky <andrei.beloboky@yandex.ru>
2026-07-27 15:15:50 +0300

    first commit

---
+++ src/test/java/com/beloboki/authentication_service/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beloboki.authentication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
