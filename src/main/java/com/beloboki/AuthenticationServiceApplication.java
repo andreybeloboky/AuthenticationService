34b9a138f0bb5e701be883fb7c3bcea65671f1a3
Andrey_Beloboky <andrei.beloboky@yandex.ru>
2026-07-27 15:28:22 +0300

    first commit

--- src/main/java/com/beloboki/AuthenticationServiceApplication.java
+++ src/main/java/com/beloboki/AuthenticationServiceApplication.java
@@ -1,4 +1,4 @@
-package com.beloboki.authentication_service;
+package com.beloboki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class AuthenticationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationServiceApplication.class, args);
 	}
-
 }
