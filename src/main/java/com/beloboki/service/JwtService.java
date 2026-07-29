4039f927eeb73030c9fa5bc11861c8f0ee147e89
Andrey_Beloboky <andrei.beloboky@yandex.ru>
2026-07-29 15:12:11 +0300

    implement generated token JWTService

--- src/main/java/com/beloboki/service/JwtService.java
+++ src/main/java/com/beloboki/service/JwtService.java
@@ -1,4 +1,34 @@
 package com.beloboki.service;
 
+import com.beloboki.model.Role;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private final SecretKey secretKey;
+
+    public JwtService(@Value("${jwt.secret}") String secret) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(String username, Long userId, Role role) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("userId", userId)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 3600_000))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
 }
