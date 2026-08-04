ceaff603e1dcb44c9646320de9a0be6f4c29a001
Andrey_Beloboky <andrei.beloboky@yandex.ru>
2026-08-04 15:28:19 +0300

    add value to WebClientConfig

--- src/main/java/com/beloboki/client/WebClientConfig.java
+++ src/main/java/com/beloboki/client/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.beloboki.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -9,6 +10,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${user-service.url:http://user-service:8080}")
+    private String userServiceUrl;
+
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
@@ -16,7 +20,7 @@ public class WebClientConfig {
 
     @Bean
     public WebClient userWebClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://user-service:8080")
+        return builder.baseUrl(userServiceUrl)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
